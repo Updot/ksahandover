0e32238bfd4c96b9ca149d33960971aca7c64002
Electromorphous <pstation.474@gmail.com>
2022-02-21 21:19:56 +0530

    scroll to top

--- src/containers/PrivacyPolicy.jsx
+++ src/containers/PrivacyPolicy.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLangContext } from "../contexts/LanguageContext";
 import EnglishPrivacyPolicy from "../components/EnglishPrivacyPolicy";
 import ArabicPrivacyPolicy from "../components/ArabicPrivacyPolicy";
 
 function PrivacyPolicy() {
   const lang = useLangContext();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
